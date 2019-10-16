10c07ba84e0815abc8ad7629536a74d34428d74f
Can Berberoglu <huseyincan@4play.com.tr>
2019-10-16 11:09:20 +0300

    initial commit

---
+++ src/main/java/io/can/userwsdemo/UserWsDemoApplication.java
@@ -0,0 +1,13 @@
+package io.can.userwsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserWsDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserWsDemoApplication.class, args);
+    }
+
+}
