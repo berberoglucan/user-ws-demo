9ccb56ba789ad846669750d58c34ae5d1289814c
Can Berberoglu <huseyincan@4play.com.tr>
2019-10-20 23:11:38 +0300

    token secrey key and expiration time carried to app properties file

--- src/main/java/io/can/userwsdemo/ProjectConstants.java
+++ src/main/java/io/can/userwsdemo/ProjectConstants.java
@@ -9,9 +9,7 @@ public class ProjectConstants {
     public static final String LOGIN_ENDPOINT = "/login";
 
     // Security Constants
-    public static final long EXPIRATION_TIME = 864000000L; // 10 days (milliseconds) for token expired
     public static final String TOKEN_PREFIX = "Bearer ";
-    public static final String TOKEN_SECRET = "jf9i4jgu83nfl0"; // encryption of the value in the access token
 
     // Response header name constants
     public static final String AUTHORIZATION_HEADER_NAME = "Authorization";
